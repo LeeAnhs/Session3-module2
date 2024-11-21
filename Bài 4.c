b9e4c1d9c95df4baeeeb3479f162a5287b7e9d64
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-21 10:48:18 +0700

    Bài 4

---
+++ Bài 4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main () {
+	float toan,van,anh;
+	
+	printf("Nhap diem Toan: ");
+	scanf("%f", &toan);
+	 
+	printf("Nhap diem Van: ");
+	scanf("%f", &van);
+	
+	printf("Nhap diem Anh: ");
+	scanf("%f", &anh);
+	
+    float  sum=toan+van+anh;
+	float tbc=sum/3;
+	 
+	printf("Tong diem 3 nam la: %.2f\n", sum);
+	printf("Diem trung binh 3 mon là: %.2f\n", tbc);
+	
+	return 0;
+} 
