768ddb110dc5d4c4d2552a0ccc9d60eab6878432
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-21 16:34:46 +0700

    Bài 3.c

--- Bài 3.c
+++ Bài 3.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 
 int main () {
+	//B1:Khai bao bien
 	float r,C,S;
 	const float PI=3.14;
-	
+	//B2:Xu ly nhap xuat du lieu
 	
 	printf("Nhap ban kinh hinh tron:");
 	scanf("%f", &r);
-	
+	//B3:Xu ly tinh toan
 	C=2*PI*r;
 	S=PI*r*r;
-	
+	//B4:Hien thi ket qua
 	printf("Chu vi la: %.2f\n", C);
 	printf("Dien tich la: %.2f\n", S);
-	
+	//B5:Ket thuc chuong trinh
 	return 0;
 	
 	
