f41a13222627db57cad78733c1e3378ebb0e6b04
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-21 21:26:22 +0700

    Bài 1.c

--- Bài 1.c
+++ Bài 1.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main () {
+	//B1:Khai bao bien
 	char name[100];
+	//B2:Nhap xuat du lieu
 	printf("Enter your name:");
 	scanf("%s", name);
+	//B3: Hien thi ket qua
 	printf("Xin chao %s\n", name);
-	
+	//B4:Ket thuc chuong trinh
 	return 0; 
 } 
